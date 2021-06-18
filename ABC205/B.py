3a78b808d4b4b42ab8b0dc694bcbbd039d28a998
kobakobashu <kobaya4@fuji.waseda.jp>
2021-06-18 23:58:05 +0900

    add B

---
+++ ABC205/B.py
@@ -0,0 +1,17 @@
+n = int(input())
+num_list = list(map(int, input().split()))
+
+num_list.sort()
+print(num_list)
+for i in range(1, len(num_list) + 1):
+    if i != num_list[i - 1]:
+        print("No")
+        exit()
+
+print("Yes")
+
+"""
+time : 2m
+time complexity : O(nlogn)
+space complexity : O(1)
+"""
